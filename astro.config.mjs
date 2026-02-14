50ee44b96e7a81ad7ef350f711433e549244e1e6
jdea182 <jdea182@wgu.edu>
2026-02-14 16:17:34 -0700

    Added APK file, updated button links, added GitHub Pages deployment file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://jdea182.github.io',
+  base: '/travowl_landing',
   vite: {
     plugins: [tailwindcss()],
   },
